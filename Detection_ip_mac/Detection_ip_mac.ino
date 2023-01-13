318db9235792f7cc555e8b602284ff46bda0099c
charlesdesenclos <75635265+charlesdesenclos@users.noreply.github.com>
2023-01-13 11:25:53 +0100

    commit 2.0

--- Detection_ip_mac/Detection_ip_mac.ino
+++ Detection_ip_mac/Detection_ip_mac.ino
@@ -1,3 +1,4 @@
+
 #include <Ethernet.h> // Inclut la bibliothèque Ethernet
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // Adresse MAC de l'Arduino
@@ -9,8 +10,8 @@ void setup() {
   // Affiche l'adresse IP et l'adresse MAC de la carte Arduino
   Serial.print("Adresse IP : ");
   Serial.println(Ethernet.localIP());
-  Serial.print("Adresse MAC : ");
-  Serial.println(Ethernet.macAddress());
+  //Serial.print("Adresse MAC : ");
+  //Serial.println(Ethernet.macAddress());
 }
 
 void loop() {
